f1004ae0c1363cd18c0848e44b41275532da70d9
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-02-05 09:47:08 +0100

    Test setup with sending initial funds

--- src/lib/testutils.spec.ts
+++ src/lib/testutils.spec.ts
@@ -1,7 +1,13 @@
 // This file outputs some basic test functionality, and includes tests that they work
+import { Random } from '@cosmjs/crypto';
+import { Bech32 } from '@cosmjs/encoding';
 import { Decimal } from '@cosmjs/math';
 import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
-import { SigningStargateClient, StargateClient } from '@cosmjs/stargate';
+import {
+  isBroadcastTxFailure,
+  SigningStargateClient,
+  StargateClient,
+} from '@cosmjs/stargate';
 import { SigningStargateClientOptions } from '@cosmjs/stargate/types/signingstargateclient';
 import test from 'ava';
 
@@ -44,7 +50,7 @@ export const wasmd = {
   denomStaking: 'ustake',
   denomFee: 'ucosm',
   blockTime: 1_000, // ms
-  facuet: {
+  faucet: {
     mnemonic:
       'enlist hip relief stomach skate base shallow young switch frequent cry park',
     pubkey0: {
@@ -76,6 +82,17 @@ interface QueryOpts {
   readonly tendermintUrlHttp: string;
 }
 
+type FundingOpts = SigningOpts & {
+  readonly faucet: {
+    readonly mnemonic: string;
+  };
+};
+
+interface SigningInfo {
+  client: SigningStargateClient;
+  address: string;
+}
+
 export async function queryClient(opts: QueryOpts): Promise<StargateClient> {
   return StargateClient.connect(opts.tendermintUrlHttp);
 }
@@ -83,12 +100,13 @@ export async function queryClient(opts: QueryOpts): Promise<StargateClient> {
 export async function signingClient(
   opts: SigningOpts,
   mnemonic: string
-): Promise<SigningStargateClient> {
+): Promise<SigningInfo> {
   const signer = await DirectSecp256k1HdWallet.fromMnemonic(
     mnemonic,
     undefined,
     opts.prefix
   );
+  const { address } = (await signer.getAccounts())[0];
   const options: SigningStargateClientOptions = {
     prefix: opts.prefix,
     gasPrice: {
@@ -96,11 +114,33 @@ export async function signingClient(
       denom: opts.denomFee,
     },
   };
-  return SigningStargateClient.connectWithSigner(
+  const client = await SigningStargateClient.connectWithSigner(
     opts.tendermintUrlHttp,
     signer,
     options
   );
+  return { address, client };
+}
+
+export async function fundAccount(
+  opts: FundingOpts,
+  rcpt: string,
+  amount: string
+): Promise<void> {
+  const { address, client } = await signingClient(opts, opts.faucet.mnemonic);
+  const feeTokens = {
+    amount,
+    denom: opts.denomFee,
+  };
+  const resp = await client.sendTokens(address, rcpt, [feeTokens]);
+  if (isBroadcastTxFailure(resp)) {
+    throw new Error(`funding failed (${resp.code}) ${resp.rawLog}`);
+  }
+}
+
+export function randomAddress(prefix: string): string {
+  const random = Random.getBytes(20);
+  return Bech32.encode(prefix, random);
 }
 
 test('query account balance - simapp', async (t) => {
@@ -117,4 +157,38 @@ test('query account balance - wasmd', async (t) => {
   t.is(account.length, 2);
   t.deepEqual(account[0], { amount: '1000000000', denom: wasmd.denomFee });
   t.deepEqual(account[1], { amount: '1000000000', denom: wasmd.denomStaking });
+});
+
+test.serial('send initial funds - simapp', async (t) => {
+  const client = await queryClient(simapp);
+  const newbie = randomAddress(simapp.prefix);
+
+  // account empty at start
+  let account = await client.getAllBalancesUnverified(newbie);
+  t.deepEqual(account, []);
+
+  // let's send some tokens
+  await fundAccount(simapp, newbie, '500');
+
+  // account has tokens
+  account = await client.getAllBalancesUnverified(newbie);
+  t.is(account.length, 1);
+  t.deepEqual(account[0], { amount: '500', denom: simapp.denomFee });
+});
+
+test.serial('send initial funds - wasmd', async (t) => {
+  const client = await queryClient(wasmd);
+  const newbie = randomAddress(wasmd.prefix);
+
+  // account empty at start
+  let account = await client.getAllBalancesUnverified(newbie);
+  t.deepEqual(account, []);
+
+  // let's send some tokens
+  await fundAccount(wasmd, newbie, '500');
+
+  // account has tokens
+  account = await client.getAllBalancesUnverified(newbie);
+  t.is(account.length, 1);
+  t.deepEqual(account[0], { amount: '500', denom: wasmd.denomFee });
 });
