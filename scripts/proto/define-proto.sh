c36baf97741e60e8fc2a2ab002e92adbc2cf9a61
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-02-09 07:33:30 +0100

    Compile protobuf types with ts-proto

--- scripts/proto/define-proto.sh
+++ scripts/proto/define-proto.sh
@@ -13,6 +13,7 @@ COSMOS_PROTO_DIR="$ROOT_PROTO_DIR/proto"
 THIRD_PARTY_PROTO_DIR="$ROOT_PROTO_DIR/third_party/proto"
 OUT_DIR="$ROOT/src/codec/"
 
+rm -rf "$OUT_DIR"
 mkdir -p "$OUT_DIR"
 
 protoc \
@@ -21,30 +22,26 @@ protoc \
   --proto_path="$COSMOS_PROTO_DIR" \
   --proto_path="$THIRD_PARTY_PROTO_DIR" \
   --ts_proto_opt="esModuleInterop=true,forceLong=long,useOptionals=true" \
-  "$COSMOS_PROTO_DIR/cosmos/auth/v1beta1/auth.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/auth/v1beta1/query.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/bank/v1beta1/bank.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/bank/v1beta1/query.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/bank/v1beta1/tx.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/base/abci/v1beta1/abci.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/base/query/v1beta1/pagination.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/base/v1beta1/coin.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/crypto/multisig/v1beta1/multisig.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/crypto/secp256k1/keys.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/distribution/v1beta1/distribution.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/distribution/v1beta1/query.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/distribution/v1beta1/tx.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/staking/v1beta1/query.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/staking/v1beta1/staking.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/staking/v1beta1/tx.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/tx/signing/v1beta1/signing.proto" \
-  "$COSMOS_PROTO_DIR/cosmos/tx/v1beta1/tx.proto" \
+  "$COSMOS_PROTO_DIR/ibc/applications/transfer/v1/transfer.proto" \
+  "$COSMOS_PROTO_DIR/ibc/applications/transfer/v1/genesis.proto" \
+  "$COSMOS_PROTO_DIR/ibc/applications/transfer/v1/query.proto" \
+  "$COSMOS_PROTO_DIR/ibc/applications/transfer/v1/tx.proto" \
   "$COSMOS_PROTO_DIR/ibc/core/channel/v1/channel.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/channel/v1/genesis.proto" \
   "$COSMOS_PROTO_DIR/ibc/core/channel/v1/query.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/channel/v1/tx.proto" \
   "$COSMOS_PROTO_DIR/ibc/core/client/v1/client.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/client/v1/genesis.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/client/v1/query.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/client/v1/tx.proto" \
   "$COSMOS_PROTO_DIR/ibc/core/commitment/v1/commitment.proto" \
   "$COSMOS_PROTO_DIR/ibc/core/connection/v1/connection.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/connection/v1/genesis.proto" \
   "$COSMOS_PROTO_DIR/ibc/core/connection/v1/query.proto" \
+  "$COSMOS_PROTO_DIR/ibc/core/connection/v1/tx.proto" \
+  "$COSMOS_PROTO_DIR/ibc/lightclients/localhost/v1/localhost.proto" \
+  "$COSMOS_PROTO_DIR/ibc/lightclients/solomachine/v1/solomachine.proto" \
+  "$COSMOS_PROTO_DIR/ibc/lightclients/tendermint/v1/tendermint.proto" \
   "$THIRD_PARTY_PROTO_DIR/confio/proofs.proto" \
   "$THIRD_PARTY_PROTO_DIR/tendermint/abci/types.proto" \
   "$THIRD_PARTY_PROTO_DIR/tendermint/crypto/keys.proto" \
