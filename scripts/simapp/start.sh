4a6254427f728dadc75f6da94e416646b423d4b3
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-02-10 21:37:10 +0100

    Quieter log output, just new blocks

--- scripts/simapp/start.sh
+++ scripts/simapp/start.sh
@@ -13,10 +13,10 @@ SCRIPT_DIR="$(realpath "$(dirname "$0")")"
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR"/env
 
-TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/simapp.XXXXXXXXX")
-chmod 777 "$TMP_DIR"
-echo "Using temporary dir $TMP_DIR"
-SIMD_LOGFILE="$TMP_DIR/simd.log"
+# TMP_DIR=$(mktemp -d "${TMPDIR:-/tmp}/simapp.XXXXXXXXX")
+# chmod 777 "$TMP_DIR"
+# echo "Using temporary dir $TMP_DIR"
+# SIMD_LOGFILE="$TMP_DIR/simd.log"
 
 # Use a fresh volume for every start
 docker volume rm -f simapp_data
@@ -30,14 +30,6 @@ docker run --rm \
   --mount type=volume,source=simapp_data,target=/root \
   "$REPOSITORY:$VERSION" \
   /template/run_simd.sh \
-  >"$SIMD_LOGFILE" &
+  2>&1 | grep 'Executed block' &
 
-echo "simd running on http://localhost:$TENDERMINT_PORT_HOST and logging into $SIMD_LOGFILE"
-
-if [ -n "${CI:-}" ]; then
-  # Give process some time to come alive. No idea why this helps. Needed for CI.
-  sleep 0.5
-
-  # Follow the logs in CI's background job
-  tail -f "$SIMD_LOGFILE"
-fi
+echo "simd running on http://localhost:$TENDERMINT_PORT_HOST"
