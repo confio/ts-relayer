1d4dc28dacbf4554e57ee38a73846ac222dc7fde
bartmacbartek <bartekconfio@gmail.com>
2021-02-23 09:02:59 +0100

    Boilerplate for the init command

---
+++ src/binary/ibc-setup/commands/init.ts
@@ -0,0 +1,58 @@
+import fs from 'fs';
+import path from 'path';
+
+import { Command } from 'commander';
+
+import { GlobalOptions } from '../types';
+
+type Options = GlobalOptions & {
+  src: string;
+  dest: string;
+};
+
+type ProgramOptions = Partial<Options>;
+
+function resolveHome(home?: string) {
+  if (home) {
+    return home;
+  }
+
+  if (!process.env.HOME) {
+    throw new Error('$HOME environment variable is not set.');
+  }
+
+  return `${process.env.HOME}/.ibc-setup`;
+}
+
+const REGISTRY_FILE = 'registry.yaml';
+
+export function init(_: unknown, program: Command) {
+  const programOptions: ProgramOptions = program.opts();
+
+  // TODO: Resolve options with precedence (env, command line etc.)
+  const options = {
+    src: programOptions.src ?? 'src',
+    dest: programOptions.dest ?? 'dest',
+    home: resolveHome(programOptions.home),
+  };
+
+  run(options);
+}
+
+function run(options: Options) {
+  if (!fs.existsSync(options.home)) {
+    fs.mkdirSync(options.home, { recursive: true });
+    console.log(`Initialized home directory at ${options.home}`);
+  } else if (!fs.lstatSync(options.home).isDirectory()) {
+    throw new Error(`${options.home} must be a directory. It is a file.`);
+  }
+
+  const REGISTRY_FILE_PATH = path.join(options.home, REGISTRY_FILE);
+  if (!fs.existsSync(REGISTRY_FILE_PATH)) {
+    // TODO: download registry.yaml from default location
+  } else if (!fs.lstatSync(REGISTRY_FILE_PATH).isFile()) {
+    throw new Error(`${REGISTRY_FILE_PATH} must be a file. It is a directory.`);
+  }
+
+  // TODO: Read registry.yaml
+}
--- src/binary/ibc-setup/index.ts
+++ src/binary/ibc-setup/index.ts
@@ -1,3 +1,24 @@
 #!/usr/bin/env node
 
-console.log('hello from ibc-setup');
+import { Command } from 'commander';
+
+import { init } from './commands/init';
+
+export const program = new Command();
+
+// TODO: fill options and commands descriptions
+program
+  .description('ibc-setup program description')
+  .option(
+    '--home <path>',
+    'home option description (default: $HOME/.ibc-setup)'
+  );
+
+program
+  .command('init')
+  .description('init command description')
+  .option('--src <chain>')
+  .option('--dest <chain>')
+  .action(init);
+
+program.parse();
