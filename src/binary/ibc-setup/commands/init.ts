97e2ac673aba989f1282a6183756153ed7bb45e9
bartmacbartek <bartekconfio@gmail.com>
2021-02-23 23:55:00 +0100

    Prioritize options

--- src/binary/ibc-setup/commands/init.ts
+++ src/binary/ibc-setup/commands/init.ts
@@ -5,6 +5,7 @@ import { Command } from 'commander';
 import yaml from 'js-yaml';
 
 import { GlobalOptions } from '../types';
+import { resolveOption } from '../utils/resolve-option';
 
 type Options = GlobalOptions & {
   src: string;
@@ -13,11 +14,7 @@ type Options = GlobalOptions & {
 
 type ProgramOptions = Partial<Options>;
 
-function resolveHome(home?: string) {
-  if (home) {
-    return home;
-  }
-
+function getDefaultHome() {
   if (!process.env.HOME) {
     throw new Error('$HOME environment variable is not set.');
   }
@@ -30,17 +27,16 @@ const REGISTRY_FILE = 'registry.yaml';
 export function init(_: unknown, program: Command) {
   const programOptions: ProgramOptions = program.opts();
 
-  // TODO: Resolve options with precedence (env, command line etc.)
   const options = {
-    src: programOptions.src ?? 'src',
-    dest: programOptions.dest ?? 'dest',
-    home: resolveHome(programOptions.home),
+    src: resolveOption('src', programOptions.src, process.env.RELAYER_SRC),
+    dest: resolveOption('dest', programOptions.dest, process.env.RELAYER_DEST),
+    home: resolveOption('home', programOptions.home, getDefaultHome),
   };
 
   run(options);
 }
 
-function run(options: Options) {
+export function run(options: Options) {
   if (!fs.existsSync(options.home)) {
     fs.mkdirSync(options.home, { recursive: true });
     console.log(`Initialized home directory at ${options.home}`);
@@ -58,7 +54,7 @@ function run(options: Options) {
   try {
     const registry = yaml.load(fs.readFileSync(REGISTRY_FILE_PATH, 'utf-8'));
 
-    // TODO: registry validation?
+    // TODO: registry file validation?
     console.log(registry);
   } catch (error) {
     throw new Error(error);
