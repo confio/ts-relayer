373fd7be195939dcf6192b37e013b58d13042f47
willclarktech <willclarktech@users.noreply.github.com>
2021-02-24 10:27:24 +0000

    Simplify LogMethod type

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,37 +1,26 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-// From https://github.com/winstonjs/winston/blob/v3.3.3/index.d.ts#L53
-type LogCallback = (
-  error?: any,
-  level?: string,
-  message?: string,
-  meta?: any
-) => void;
-
-// From https://github.com/winstonjs/winston/blob/v3.3.3/index.d.ts#L69-L75
-export interface LeveledLogMethod {
-  (message: string, callback: LogCallback): Logger;
-  (message: string, meta: any, callback: LogCallback): Logger;
-  (message: string, ...meta: any[]): Logger;
-  (message: any): Logger;
-  (infoObject: Record<string, unknown>): Logger;
-}
+// Adapted from https://github.com/winstonjs/winston/blob/v3.3.3/index.d.ts#L69-L75
+export type LogMethod = (
+  /* The string to be logged */
+  message: string,
+  /* Optional object to be JSON-stringified by the logger */
+  meta?: Record<string, unknown>
+) => Logger;
 
 // Logger interface with a subset of methods from https://github.com/winstonjs/winston/blob/v3.3.3/index.d.ts#L107-L115
 export interface Logger {
-  error: LeveledLogMethod;
-  warn: LeveledLogMethod;
-  info: LeveledLogMethod;
-  verbose: LeveledLogMethod;
-  debug: LeveledLogMethod;
+  error: LogMethod;
+  warn: LogMethod;
+  info: LogMethod;
+  verbose: LogMethod;
+  debug: LogMethod;
 }
 
 export class NoopLogger implements Logger {
-  public readonly error: LeveledLogMethod;
-  public readonly warn: LeveledLogMethod;
-  public readonly info: LeveledLogMethod;
-  public readonly verbose: LeveledLogMethod;
-  public readonly debug: LeveledLogMethod;
+  public readonly error: LogMethod;
+  public readonly warn: LogMethod;
+  public readonly info: LogMethod;
+  public readonly verbose: LogMethod;
+  public readonly debug: LogMethod;
 
   constructor() {
     this.error = () => this;
