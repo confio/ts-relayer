7c0d629edfaa99c54f1946ea8031b4854e835b85
bartmacbartek <bartekconfio@gmail.com>
2021-02-24 16:47:39 +0100

    Format test

--- src/binary/ibc-setup/commands/init.spec.ts
+++ src/binary/ibc-setup/commands/init.spec.ts
@@ -12,16 +12,47 @@ const fsMkdirSync = sinon.stub(fs, 'mkdirSync');
 const axiosGet = sinon.stub(axios, 'get');
 const fsReadFileSync = sinon.stub(fs, 'readFileSync');
 const fsWriteFileSync = sinon.stub(fs, 'writeFileSync');
-
 const fsLstatSync = sinon.stub(fs, 'lstatSync');
 
 test.beforeEach(() => {
   sinon.reset();
+
   fsLstatSync.returns({
     ...new fs.Stats(),
     isDirectory: () => true,
     isFile: () => true,
   });
+});
+
+test('read existing registry.yaml', async (t) => {
+  const options: Options = {
+    home: '/home/user',
+    src: 'AAA',
+    dest: 'BBB',
+  };
+
+  fsExistSync.onCall(0).returns(true).onCall(1).returns(true);
+
+  const registryPath = `${options.home}/registry.yaml`;
+  const registryYaml = `
+  version: 1
+  `;
+
+  axiosGet.resolves({
+    data: registryYaml,
+  });
+  fsReadFileSync.returns(registryYaml);
+  fsWriteFileSync.returns(undefined);
+
+  await run(options);
+
+  t.assert(fsExistSync.calledWithExactly(options.home));
+  t.assert(fsExistSync.calledWithExactly(registryPath));
+  t.assert(fsMkdirSync.notCalled);
+  t.assert(axiosGet.notCalled);
+  t.assert(fsReadFileSync.calledOnceWith(registryPath));
+  t.assert(fsWriteFileSync.notCalled);
+  t.assert(consoleLog.calledWith({ version: 1 }));
 });
 
 test('initialize home directory and pull registry.yaml from remote', async (t) => {
@@ -54,36 +85,5 @@ test('initialize home directory and pull registry.yaml from remote', async (t) =
   t.assert(fsReadFileSync.calledOnceWith(registryPath));
   t.assert(fsWriteFileSync.calledOnceWith(registryPath, registryYaml));
   t.assert(consoleLog.calledWithMatch(new RegExp(`at ${options.home}`)));
-  t.assert(consoleLog.calledWith({ version: 1 }));
-});
-
-test('read existing registry.yaml', async (t) => {
-  const options: Options = {
-    home: '/home/user',
-    src: 'AAA',
-    dest: 'BBB',
-  };
-
-  fsExistSync.onCall(0).returns(true).onCall(1).returns(true);
-
-  const registryPath = `${options.home}/registry.yaml`;
-  const registryYaml = `
-  version: 1
-  `;
-
-  axiosGet.resolves({
-    data: registryYaml,
-  });
-  fsReadFileSync.returns(registryYaml);
-  fsWriteFileSync.returns(undefined);
-
-  await run(options);
-
-  t.assert(fsExistSync.calledWithExactly(options.home));
-  t.assert(fsExistSync.calledWithExactly(registryPath));
-  t.assert(fsMkdirSync.notCalled);
-  t.assert(axiosGet.notCalled);
-  t.assert(fsReadFileSync.calledOnceWith(registryPath));
-  t.assert(fsWriteFileSync.notCalled);
   t.assert(consoleLog.calledWith({ version: 1 }));
 });
