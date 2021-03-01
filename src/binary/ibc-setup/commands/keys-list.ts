dc9e4428862a1a336871972c98504a3fd2067f22
bartmacbartek <bartekconfio@gmail.com>
2021-03-01 23:07:45 +0100

    Logic for keys list command

--- src/binary/ibc-setup/commands/keys-list.ts
+++ src/binary/ibc-setup/commands/keys-list.ts
@@ -1,26 +1,93 @@
-import { getDefaultHomePath } from '../utils/get-default-home-path';
-import { resolveOption } from '../utils/resolve-option';
+import fs from 'fs';
+import os from 'os';
+import path from 'path';
+import readline from 'readline';
 
-export type Options = {
+import { registryFile } from '../../constants';
+import { deriveAddress } from '../utils/derive-address';
+import { getDefaultHomePath } from '../utils/get-default-home-path';
+import { loadAndValidateRegistry } from '../utils/load-and-validate-registry';
+import { resolveRequiredOption } from '../utils/resolve-required-option';
+
+type Flags = {
+  readonly interactive: boolean;
   readonly home?: string;
   readonly keyFile?: string;
   readonly mnemonic?: string;
-  readonly interactive?: boolean;
 };
 
-export function keysList(flags: Options) {
-  console.log(flags);
-  const options = {
-    home: resolveOption(
+type Options = {
+  readonly home: string;
+  readonly mnemonic: string;
+};
+
+export async function readMnemonicFromStdin(interactive: Flags['interactive']) {
+  if (!interactive) {
+    return undefined;
+  }
+
+  const readlineInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const mnemonic = await new Promise<string>((resolve) => {
+    readlineInterface.question('enter mnemonic phrase: ', (stdin) => {
+      readlineInterface.close();
+      resolve(stdin);
+    });
+  });
+
+  return mnemonic;
+}
+
+function readMnemonicFromFile(keyFile: Flags['keyFile']) {
+  if (!keyFile) {
+    return undefined;
+  }
+
+  return () => {
+    return fs.readFileSync(keyFile, 'utf-8').trim();
+  };
+}
+
+export async function keysList(flags: Flags) {
+  const options: Options = {
+    home: resolveRequiredOption('home')(
       flags.home,
       process.env.RELAYER_HOME,
       getDefaultHomePath
     ),
+    mnemonic: resolveRequiredOption('mnemonic')(
+      await readMnemonicFromStdin(flags.interactive),
+      flags.mnemonic,
+      readMnemonicFromFile(flags.keyFile),
+      process.env.RELAYER_MNEMONIC
+    ),
   };
 
-  run(options);
+  await run(options);
 }
 
-export function run(options: Options) {
-  console.log(options);
+export async function run(options: Options) {
+  const registryFilePath = path.join(options.home, registryFile);
+  const registry = loadAndValidateRegistry(registryFilePath);
+
+  const chains = Object.entries(registry.chains);
+
+  const addresses = (
+    await Promise.all(
+      chains.map(([, data]) =>
+        deriveAddress(options.mnemonic, data.prefix, data.hd_path)
+      )
+    )
+  )
+    .map((address, index) => {
+      const chain = chains[index][0];
+
+      return `${chain}: ${address}`;
+    })
+    .join(os.EOL);
+
+  console.log(addresses);
 }
