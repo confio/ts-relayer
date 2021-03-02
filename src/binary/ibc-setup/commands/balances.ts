d0ef1bb4e422aecc87c3b0c6f93fd135511819cf
bartmacbartek <bartekconfio@gmail.com>
2021-03-02 23:21:28 +0100

    Logic for balances command

--- src/binary/ibc-setup/commands/balances.ts
+++ src/binary/ibc-setup/commands/balances.ts
@@ -1,9 +1,11 @@
-import os from 'os';
 import path from 'path';
 
+import { GasPrice } from '@cosmjs/launchpad';
+import { Decimal } from '@cosmjs/math';
+import { DirectSecp256k1HdWallet } from '@cosmjs/proto-signing';
+
+import { IbcClient } from '../../../lib/ibcclient';
 import { registryFile } from '../../constants';
-import { Chain } from '../types';
-import { deriveAddress } from '../utils/derive-address';
 import { getDefaultHomePath } from '../utils/get-default-home-path';
 import { loadAndValidateApp } from '../utils/load-and-validate-app';
 import { loadAndValidateRegistry } from '../utils/load-and-validate-registry';
@@ -11,29 +13,20 @@ import { resolveMnemonicOption } from '../utils/resolve-mnemonic-option';
 import { resolveOption } from '../utils/resolve-option';
 import { resolveRequiredOption } from '../utils/resolve-required-option';
 
-export type Flags = {
-  readonly interactive: boolean;
-  readonly home?: string;
-  readonly keyFile?: string;
-  readonly mnemonic?: string;
-};
+import { Flags, getAddresses, Options } from './keys-list';
 
-export type Options = {
-  readonly home: string;
-  readonly mnemonic: string;
-};
-
-export async function keysList(flags: Flags) {
+export async function balances(flags: Flags) {
   const home = resolveRequiredOption('home')(
     flags.home,
     process.env.RELAYER_HOME,
     getDefaultHomePath
   );
   const app = loadAndValidateApp(home);
+
   const keyFile = resolveOption(
     flags.keyFile,
-    process.env.KEY_FILE,
-    app?.keyFile
+    app?.keyFile,
+    process.env.KEY_FILE
   );
 
   const options: Options = {
@@ -49,28 +42,33 @@ export async function keysList(flags: Flags) {
   await run(options);
 }
 
-export async function getAddresses(
-  registryChains: Record<string, Chain>,
-  mnemonic: string
-): Promise<[chain: string, data: Chain, address: string][]> {
-  const chains = Object.entries(registryChains);
-
-  return (
-    await Promise.all(
-      chains.map(([, data]) =>
-        deriveAddress(mnemonic, data.prefix, data.hd_path)
-      )
-    )
-  ).map((address, index) => [chains[index][0], chains[index][1], address]);
-}
-
 export async function run(options: Options) {
   const registryFilePath = path.join(options.home, registryFile);
   const registry = loadAndValidateRegistry(registryFilePath);
 
-  const addresses = (await getAddresses(registry.chains, options.mnemonic))
-    .map(([chain, , address]) => `${chain}: ${address}`)
-    .join(os.EOL);
+  const addresses = await getAddresses(registry.chains, options.mnemonic);
 
-  console.log(addresses);
+  for (const [chain, data, address] of addresses) {
+    const signer = await DirectSecp256k1HdWallet.fromMnemonic(
+      options.mnemonic,
+      undefined,
+      data.prefix
+    );
+
+    const gasPrice = GasPrice.fromString(data.gas_price);
+
+    const client = await IbcClient.connectWithSigner(
+      data.rpc[0], // rpc[0] is guaranteed to be defined by registry validator
+      signer,
+      address,
+      {
+        prefix: data.prefix,
+        gasPrice,
+      }
+    );
+
+    const balances = await client.query.bank.unverified.allBalances(address);
+
+    console.log(chain, balances);
+  }
 }
