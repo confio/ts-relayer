1b2c21d05b5bc19e6fa1d1d1b2c1e15faaaa2d01
bartmacbartek <bartekconfio@gmail.com>
2021-03-02 21:26:55 +0100

    Throw app validation errors

--- src/binary/ibc-setup/utils/load-and-validate-app.ts
+++ src/binary/ibc-setup/utils/load-and-validate-app.ts
@@ -1,4 +1,5 @@
-import fs from 'fs';
+import fs, { appendFileSync } from 'fs';
+import os from 'os';
 import path from 'path';
 
 import Ajv, { JSONSchemaType } from 'ajv';
@@ -8,29 +9,38 @@ import { appFile } from '../../constants';
 import { App } from '../types';
 
 export function loadAndValidateApp(home: string) {
-  try {
-    const app = yaml.load(fs.readFileSync(path.join(home, appFile), 'utf-8'));
+  const appFilePath = path.join(home, appFile);
 
-    const ajv = new Ajv({ allErrors: true });
-    const schema: JSONSchemaType<App> = {
-      type: 'object',
-      additionalProperties: false,
-      required: [],
-      properties: {
-        src: { type: 'string', nullable: true, default: null },
-        dest: { type: 'string', nullable: true },
-        mnemonic: { type: 'string', nullable: true },
-        keyFile: { type: 'string', nullable: true },
-      },
-    };
-    const validate = ajv.compile(schema);
-
-    if (!validate(app)) {
-      return null;
-    }
-
-    return app;
-  } catch {
+  if (!fs.existsSync(appFilePath)) {
     return null;
   }
+
+  if (!fs.lstatSync(appFilePath).isFile()) {
+    throw new Error(`${appendFileSync} must be a file.`);
+  }
+
+  const app = yaml.load(fs.readFileSync(path.join(home, appFile), 'utf-8'));
+
+  const ajv = new Ajv({ allErrors: true });
+  const schema: JSONSchemaType<App> = {
+    type: 'object',
+    additionalProperties: false,
+    required: [],
+    properties: {
+      src: { type: 'string', nullable: true, default: null },
+      dest: { type: 'string', nullable: true },
+      mnemonic: { type: 'string', nullable: true },
+      keyFile: { type: 'string', nullable: true },
+    },
+  };
+  const validate = ajv.compile(schema);
+
+  if (!validate(app)) {
+    const errors = (validate.errors ?? []).map(
+      ({ dataPath, message }) => `"${dataPath}" ${message}`
+    );
+    throw new Error([`${appFile} validation failed.`, ...errors].join(os.EOL));
+  }
+
+  return app;
 }
