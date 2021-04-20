5682944720c9cb959b8a89e70ae8a49dc0452f5c
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-04-20 15:31:46 +0200

    Rough attempt to copy simapp to gaia

---
+++ scripts/gaia/start.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+command -v shellcheck >/dev/null && shellcheck "$0"
+
+# Please keep this in sync with the Ports overview in HACKING.md
+TENDERMINT_PORT_GUEST="26657"
+TENDERMINT_PORT_HOST="26659"
+API_PORT_GUEST="1317"
+API_PORT_HOST="1319"
+
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+# shellcheck source=./env
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR"/env
+
+# Use a fresh volume for every start
+docker volume rm -f gaia_data
+docker pull "$REPOSITORY:$VERSION"
+
+echo "starting gaiad running on http://localhost:$TENDERMINT_PORT_HOST"
+
+docker run --rm \
+  --name "$CONTAINER_NAME" \
+  -p "$TENDERMINT_PORT_HOST":"$TENDERMINT_PORT_GUEST" \
+  -p "$API_PORT_HOST":"$API_PORT_GUEST" \
+  --mount type=bind,source="$SCRIPT_DIR/template",target=/template \
+  --mount type=volume,source=gaia_data,target=/root \
+  "$REPOSITORY:$VERSION" \
+  /template/run_gaiad.sh \
+  2>&1 | grep 'Executed block'
