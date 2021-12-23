6c8d81c9278a778e6bf3bf5aa08b68006372f7eb
Ethan Frey <ethanfrey@users.noreply.github.com>
2021-12-23 18:29:31 +0100

    Generate new v6.0.0 template

--- scripts/gaia/generate_template.sh
+++ scripts/gaia/generate_template.sh
@@ -3,6 +3,7 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+echo $SCRIPT_DIR
 # shellcheck source=./env
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR"/env
@@ -19,9 +20,9 @@ docker run --rm \
   --user=root \
   -e PASSWORD=my-secret-password \
   -e STAKE_TOKEN=uatom \
-  --mount type=bind,source="$SCRIPT_DIR/template",target=/gaia \
+  --mount type=bind,source="$SCRIPT_DIR/template",target=/root \
   "$REPOSITORY:$VERSION" \
-  ./setup.sh \
+  /root/setup.sh \
   cosmos1pkptre7fdkl6gfrzlesjjvhxhlc3r4gmmk8rs6 cosmos10dyr9899g6t0pelew4nvf4j5c3jcgv0r73qga5 cosmos1xy4yqngt0nlkdcenxymg8tenrghmek4nmqm28k cosmos142u9fgcjdlycfcez3lw8x6x5h7rfjlnfhpw2lx cosmos1hsm76p4ahyhl5yh3ve9ur49r5kemhp2r0dcjvx \
   cosmos14qemq0vw6y3gc3u3e0aty2e764u4gs5le3hada cosmos1hhg2rlu9jscacku2wwckws7932qqqu8x3gfgw0 cosmos1xv9tklw7d82sezh9haa573wufgy59vmwe6xxe5 cosmos17yg9mssjenmc3jkqth6ulcwj9cxujrxxzezwta cosmos1f7j7ryulwjfe9ljplvhtcaxa6wqgula3etktce \
   cosmos1lvrwcvrqlc5ktzp2c4t22xgkx29q3y83lktgzl cosmos1vkv9sfwaak76weyamqx0flmng2vuquxqcuqukh cosmos106jwym4s9aujcmes26myzzwqsccw09sdm0v5au cosmos1c7wpeen2uv8thayf7g8q2rgpm29clj0dgrdtzw cosmos1mjxpv9ft30wer7ma7kwfxhm42l379xutplrdk6 \
