fe8983ea8b5398364bd483a62ec390715c69e56b
Ethan Frey <ethanfrey@users.noreply.github.com>
2022-07-20 21:35:47 +0200

    Osmosis uses new ci image

--- scripts/osmosis/README.md
+++ scripts/osmosis/README.md
@@ -1,10 +1,29 @@
-# Local Osmosis development network
+# Local Osmosisd development network
+
+Configuration is in the `env` file, that is the most likely place you want to adjust
+
+## Initializing new data
+
+```bash
+scripts/osmosis/generate_template.sh
+```
+
+Note that the addresses receiving tokens in genesis are set here, you can customize by editting this file
 
 ## Starting the blockchain
 
 Run the following:
 
+```bash
+scripts/osmosis/start.sh
 ```
-cd scripts/osmosis
-./start.sh
+
+You get filtered output on the console. If it crashes and you want the full logs, look at `debug.log`.
+
+## Stopping the blockchain
+
+While running the blockchain in one terminal, open a second one and run this:
+
+```bash
+scripts/osmosis/stop.sh
 ```
--- scripts/osmosis/generate_template.sh
+++ scripts/osmosis/generate_template.sh
@@ -8,22 +8,17 @@ echo $SCRIPT_DIR
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR"/env
 
-rm -rf "$SCRIPT_DIR/template"
-mkdir "$SCRIPT_DIR/template"
-cp setup.sh "$SCRIPT_DIR/template/"
-chmod +x "$SCRIPT_DIR/template/setup.sh"
-cp run_osmosisd.sh "$SCRIPT_DIR/template/"
-chmod +x "$SCRIPT_DIR/template/run_osmosisd.sh"
+mkdir -p "$SCRIPT_DIR"/template
+
+echo "Trying docker $REPOSITORY:$VERSION"
 
 # The usage of the accounts below is documented in README.md of this directory
 docker run --rm \
   --user=root \
-  -e PASSWORD=my-secret-password \
-  -e STAKE_TOKEN=uosmo \
   -e TRANSFER_PORT=transfer \
   --mount type=bind,source="$SCRIPT_DIR/template",target=/root \
-  --entrypoint /root/setup.sh \
   "$REPOSITORY:$VERSION" \
+  /opt/setup.sh \
   osmo1lvrwcvrqlc5ktzp2c4t22xgkx29q3y83hdcc5d
 
 sudo chmod -R g+rwx template/.osmosisd/
@@ -34,31 +29,3 @@ sudo chmod -R a+rx template/.osmosisd/
 if [ ! -x "$SCRIPT_DIR/template/.osmosisd/config/gentx" ]; then
   sudo chown -R "$(id -u):$(id -g)" "$SCRIPT_DIR/template"
 fi
-
-(
-  cd "$SCRIPT_DIR"
-  # so weird, but found I needed the -M flag after lots of debugging odd error messages
-  # happening when redirecting stdout
-  jq -S -M . < "template/.osmosisd/config/genesis.json" > genesis.tmp
-  mv genesis.tmp "template/.osmosisd/config/genesis.json"
-  chmod a+rx template/.osmosisd/config/genesis.json
-
-  # Custom settings in config.toml
-  sed -i"" \
-    -e 's/^cors_allowed_origins =.*$/cors_allowed_origins = ["*"]/' \
-    -e 's/^timeout_propose =.*$/timeout_propose = "100ms"/' \
-    -e 's/^timeout_propose_delta =.*$/timeout_propose_delta = "100ms"/' \
-    -e 's/^timeout_prevote =.*$/timeout_prevote = "100ms"/' \
-    -e 's/^timeout_prevote_delta =.*$/timeout_prevote_delta = "100ms"/' \
-    -e 's/^timeout_precommit =.*$/timeout_precommit = "100ms"/' \
-    -e 's/^timeout_precommit_delta =.*$/timeout_precommit_delta = "100ms"/' \
-    -e 's/^timeout_commit =.*$/timeout_commit = "200ms"/' \
-    "template/.osmosisd/config/config.toml"
-
-  # Custom settings app.toml
-  sed -i"" \
-    -e 's/^enable =.*$/enable = true/' \
-    -e 's/^enabled-unsafe-cors =.*$/enabled-unsafe-cors = true/' \
-    -e 's/^minimum-gas-prices = \".*\"/minimum-gas-prices = \"0uosmo\"/' \
-    "template/.osmosisd/config/app.toml"
-)
