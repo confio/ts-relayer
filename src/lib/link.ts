907524949329a7914eb50ed365a48a000f21afa5
Clockwork <alex@clockwork.gr>
2023-11-28 12:03:59 +0200

    chore: fix linting

--- src/lib/link.ts
+++ src/lib/link.ts
@@ -37,10 +37,10 @@ export function otherSide(side: Side): Side {
     return 'A';
   }
 }
+
 /**
  * PacketFilter is the type for a function that accepts a Packet and returns a boolean defining whether to relay the packet or not
  */
-
 export type PacketFilter = (packet: Packet) => boolean;
 
 // This records the block heights from the last point where we successfully relayed packets.
